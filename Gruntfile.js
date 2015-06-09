d52eb2d930fc2dfe112e36ffa995f2c74073e1e4
Robert Dale Smith <robert@motelabs.com>
2015-06-08 23:11:48 -0500

    test2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,22 @@
 module.exports = function(grunt) {
+
+  // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     uglify: {
-      core: {
-        files: [{
-          expand: true,
-          cwd: 'src/main/resources',
-          src: 'public/js/**/*.js',
-          dest: "target/classes",
-          ext: '.js'
-        }]
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'public/js/main.js',
+        dest: 'build/main.min.js'
       }
     }
   });
 
+  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
   grunt.registerTask('default', ['uglify']);
 };
