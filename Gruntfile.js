5bed433522858979715e0f6db0ef900c9419899a
Robert Dale Smith <robert@motelabs.com>
2015-06-09 00:03:25 -0500

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
       },
       build: {
         src : 'public/js/**/*.js',
-        dest: 'public/js/main.js'
+        dest: 'public/js/script.js'
       }
     }
   });
