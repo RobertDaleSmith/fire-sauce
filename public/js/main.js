9aa8e137fe381c8a627d77bb0c54f52089dd71cd
Robert Dale Smith <robert@robertdalesmith.com>
2019-05-16 15:49:07 -0500

    only show try extension cta on desktop chrome

--- public/js/main.js
+++ public/js/main.js
@@ -232,10 +232,48 @@ $(window).bind("load", function() {
 
     loadPopularSuggestions();
 
+    checkExtCompatibility();
+
     fadeSplash();
 
 });
 
+function isChrome() {
+	var isChromium = window.chrome;
+	var winNav = window.navigator;
+	var vendorName = winNav.vendor;
+	var isOpera = typeof window.opr !== "undefined";
+	var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+	var isIOSChrome = winNav.userAgent.match("CriOS");
+
+	if (isIOSChrome) {
+		// is Google Chrome on IOS
+		return true;
+	} else if (
+		isChromium !== null &&
+		typeof isChromium !== "undefined" &&
+		vendorName === "Google Inc." &&
+		isOpera === false &&
+		isIEedge === false
+	) {
+		// is Google Chrome
+		return true;
+	} else {
+		// not Google Chrome
+		return false;
+	}
+}
+
+function isDesktop() {
+	var isTouchDevice = function() {  return 'ontouchstart' in window || 'onmsgesturechange' in window; };
+	return window.screenX != 0 && !isTouchDevice() ? true : false;
+}
+
+function checkExtCompatibility() {
+	if (isDesktop() && isChrome()) {
+		$('.extension').css('display', '');
+	}
+}
 
 function fadeSplash(){
 	setTimeout(function(){
